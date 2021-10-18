c3f6fefe01d04f22bf7d365dbd7f700c90fdeb66
ART <pakkawat.kh@gmail.com>
2021-10-19 00:40:50 +0700

    add data movie

---
+++ bo_movieadddata.php
@@ -0,0 +1,43 @@
+<?php
+require_once('connection.php');
+$_POST = json_decode(file_get_contents("php://input"),true);
+
+$nameEng = $_POST['nameEng'];
+$nameTh = $_POST['nameTh'];
+$year = $_POST['year'];
+$mparate = $_POST['mparate'];
+$durationHour = $_POST['durationHour'];
+$durationMin = $_POST['durationMin'];
+$type = $_POST['type'];
+$synopsis = $_POST['synopsis'];
+$movieCodeEng = $_POST['movieCodeEng'];
+$movieCodeTh = $_POST['movieCodeTh'];
+$trailerCode = $_POST['trailerCode'];
+$netflix = $_POST['netflix'];
+$disney = $_POST['disney'];
+$amazon = $_POST['amazon'];
+$hbo = $_POST['hbo'];
+$new = $_POST['new'];
+$expiredate = $_POST['expiredate'];
+
+$db->insert("movie",[
+    "nameEng"=>$nameEng,
+    "nameTh"=>$nameTh,
+    "year"=>$year,
+    "mparate"=>$mparate,
+    "durationHour"=>$durationHour,
+    "durationMin"=>$durationMin,
+    "type"=>$type,
+    "synopsis"=>$synopsis,
+    "movieCodeEng"=>$movieCodeEng,
+    "movieCodeTh"=>$movieCodeTh,
+    "trailerCode"=>$trailerCode,
+    "netflix"=>$netflix,
+    "disney"=>$disney,
+    "amazon"=>$amazon,
+    "hbo"=>$hbo,
+    "new"=>$new,
+    "expiredate"=>$expiredate
+]);
+echo "finish";
+?>
