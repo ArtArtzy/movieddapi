6eb059694982d46dc8dc4dc4c9052f7b7dfc2d65
ART <pakkawat.kh@gmail.com>
2021-10-19 11:55:17 +0700

    update

---
+++ bo_movieaddcat.php
@@ -0,0 +1,11 @@
+<?php
+require_once('connection.php');
+$_POST = json_decode(file_get_contents("php://input"),true);
+$catid = $_POST['catid'];
+$db->update("category",[
+"movie[+]"=>1
+],[
+"id"=>$catid
+]);
+
+?>
