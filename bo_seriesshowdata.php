0899f29471e48c37b46be154810b5564683b312f
ART <pakkawat.kh@gmail.com>
2021-10-20 17:10:32 +0700

    api

---
+++ bo_seriesshowdata.php
@@ -0,0 +1,17 @@
+<?php
+// แสดงข้อมูลเป็นหน้า
+require_once('connection.php');
+$_POST = json_decode(file_get_contents("php://input"),true);
+$cat= $_POST['catName'];
+$page = $_POST["pagedata"];
+$pagestart = ($page-1)*10;
+// echo $pagestart;
+$pageend = ($page)*10;
+if($cat == 0){
+    $sql = "select * from series limit " . $pagestart . " , " . $pageend;
+} else {
+    $sql = "select * from series where  type like '%[" .  $cat . "]%' limit ". $pagestart . " , " . $pageend;
+}
+$result  = $db->query($sql)->fetchAll();
+echo json_encode($result);
+?> 
