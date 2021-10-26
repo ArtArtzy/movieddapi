c983ae45f3550b121fdc005bc1c0562b88ec0a65
ART <pakkawat.kh@gmail.com>
2021-10-26 13:00:53 +0700

    report bug

--- bo_moviereportproblem.php
+++ bo_moviereportproblem.php
@@ -3,6 +3,7 @@
 require_once('connection.php');
 $_POST = json_decode(file_get_contents("php://input"),true);
 $id = $_POST['id'];
-$id =16;
-
+$sql = "select  problemid,problem, count(reportproblemmovie.id) as noproblem from reportproblemmovie inner join problem on reportproblemmovie.problemid = problem.id  where movieid = ". $id . " and solved = 0 group by problemid";
+$result = $db->query($sql)->fetchAll();
+echo json_encode($result);
 ?>
