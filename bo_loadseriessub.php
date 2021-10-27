ba4a84355ff1fec996a11d2305ad2a7ed276a76f
Autarn Sukonnakorn <one.at.once@gmail.com>
2021-10-27 10:24:47 +0700

    load & add series

---
+++ bo_loadseriessub.php
@@ -0,0 +1,12 @@
+<?php
+require_once('connection.php');
+$_POST = json_decode(file_get_contents("php://input"),true);
+
+$seriesid = $_POST['seriesid'];
+$seasonid = $_POST['seasonid'];
+$result=$db->select("seriessub","*",[
+    "seriesid"=>$seriesid,
+    "seasonid"=>$seasonid
+]);
+echo json_encode($result);
+?>
