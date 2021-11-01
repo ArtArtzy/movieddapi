c295edb3743ac8f0bd5c7eeca82b0fd7f52f49f7
Autarn Sukonnakorn <one.at.once@gmail.com>
2021-11-01 16:35:10 +0700

    stat

--- bo_streamingthisweek.php
+++ bo_streamingthisweek.php
@@ -1,11 +1,11 @@
 <?php
 require_once('connection.php');
 $_POST = json_decode(file_get_contents("php://input"),true);
-$currentTime=$_POST['currentTime'];
-$sql="SELECT SUM(view) as s1 FROM viewmovie where week=".$currentTime ;
+$currentW=$_POST['currentW'];
+$sql="SELECT SUM(view) as s1 FROM viewmovie where week=".$currentW ;
 $result  = $db->query($sql)->fetchAll();
 $v1=$result[0][0];
-$sql2="SELECT SUM(view) as s2 FROM viewseries where week=".$currentTime;
+$sql2="SELECT SUM(view) as s2 FROM viewseries where week=".$currentW;
 $result2=$db->query($sql2)->fetchAll();
 $v2=$result2[0][0];
 echo $v1+$v2;
