99611896c89207ca9de489307a28b4347e5ffa17
ART <pakkawat.kh@gmail.com>
2021-11-29 07:41:04 +0700

    report problem finish

--- bo_moviereportproblemclear.php
+++ bo_moviereportproblemclear.php
@@ -2,17 +2,19 @@
 // ใช้เพื่อหาจำนวน page ที่มีทั้งหมดของ movie
 require_once('connection.php');
 $_POST = json_decode(file_get_contents("php://input"),true);
-$type = $_POST['type'];
 $id = $_POST['movieid'];
-$result = $db->select("reportproblemmovieetc","*",[
-    "movieid"=>$id,
-    "solve"=>0,
-    "type"=>$type
+$type = $_POST['type'];
+if($type == 2){
+$db->update("movie",[
+    "alertThaiSub"=>0
+], [
+    "id"=>$id
 ]);
-if(sizeof($result)>0){
-    echo json_encode($result);
 } else {
-    echo "no data";
+    $db->update("movie",[
+        "alertThaiSound"=>0
+    ], [
+        "id"=>$id
+    ]); 
 }
-
 ?>
