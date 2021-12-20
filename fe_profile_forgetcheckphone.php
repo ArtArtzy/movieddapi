b22ca0819c3b9534eb92ef1f825c8a84e28e0723
Autarn Sukonnakorn <one.at.once@gmail.com>
2021-12-20 12:27:54 +0700

    decode telephone - -

--- fe_profile_forgetcheckphone.php
+++ fe_profile_forgetcheckphone.php
@@ -3,12 +3,16 @@ require_once('connection.php');
 $_POST = json_decode(file_get_contents("php://input"),true);
 
 $telephone=$_POST['telephone'];
+$telephone="097-879-8855";
+$telephone=substr_replace($telephone,substr($telephone,4),3);
+$telephone=substr_replace($telephone,substr($telephone,7),6);
 
+echo $telephone;
 $result=$db->select("user","id",[
     "telephone"=>$telephone
 ]);
 if(sizeof($result)>0){
-    echo json_encode($result2);
+    echo json_encode($result);
 }
 else{
     echo "fail";
