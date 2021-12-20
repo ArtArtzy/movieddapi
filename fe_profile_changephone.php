b5dd1e3e8bc3f8e409ec5e27492f8583feaae607
Autarn Sukonnakorn <one.at.once@gmail.com>
2021-12-20 11:16:36 +0700

    change phonenumber

--- fe_profile_changephone.php
+++ fe_profile_changephone.php
@@ -5,12 +5,25 @@ $_POST = json_decode(file_get_contents("php://input"),true);
 $userid=$_POST['userid'];
 $password=$_POST['password'];
 $newphone=$_POST['newphone'];
+$userid=19;
+$password="1122";
+$newphone="987654321";
 
-$sql="select password from user where id=" . $userid;
-$result  = $db->query($sql)->fetchAll();
+$result=$db->select("user","password",[
+    "id"=>$userid
+]);
 if(sizeof($result)>0){
-    if($password==$result[0]['password']){
-        echo "Password correct";
+    if($password==$result[0]){
+        // echo $newphone;
+        // $sql="update user set telephone='" . $newphone . "' where id=" . $userid;
+        // $result2  = $db->query($sql)->fetchAll();
+
+        $db->update("user",[
+            "telephone"=>$newphone
+        ],[
+            "id"=>$userid
+        ]);
+        echo "update";
     }
     else{
         echo "Password incorrect";
