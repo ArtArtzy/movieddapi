04b9d549a5da55f6a3ebd7a2d2743c6205adfe90
Autarn Sukonnakorn <one.at.once@gmail.com>
2021-12-20 11:25:50 +0700

    load phone xxx-xxx-xxxx

--- fe_profile_loadtelephone.php
+++ fe_profile_loadtelephone.php
@@ -3,14 +3,16 @@ require_once('connection.php');
 $_POST = json_decode(file_get_contents("php://input"),true);
 
 $userid=$_POST['userid'];
-// $userid=19;
+$userid=18;
 
 $sql="select telephone from user where id=" . $userid;
 $result  = $db->query($sql)->fetchAll();
 // echo $sql;
 
 if(sizeof($result)>0){
-    echo $result[0]['telephone'];
+    // echo $result[0]['telephone'];
+    $sendPhone=substr($result[0]['telephone'],0,3). '-' . substr($result[0]['telephone'],3,3) . '-' .substr($result[0]['telephone'],6,4);
+    echo $sendPhone;
 }
 else{
     echo "fail";
