e785065633ac1eaabfd756cac8b0209caa0e5b98
Autarn Sukonnakorn <one.at.once@gmail.com>
2021-12-20 13:01:56 +0700

    signup adduser

---
+++ fe_signup_adduser.php
@@ -0,0 +1,38 @@
+<?php
+require_once('connection.php');
+$_POST = json_decode(file_get_contents("php://input"),true);
+
+$username=$_POST['username'];
+$password=$_POST['password'];
+$telephone=$_POST['telephone'];
+$username="abcdefg";
+$password="12345678";
+$telephone="0547893256";
+
+
+
+$result=$db->select("user","id",[
+    "telephone"=>$telephone,
+]);
+if(sizeof($result)>0){
+    echo "This phone number exist";
+}
+else{
+    $result2=$db->select("user","id",[
+        "username"=>$username,
+    ]);
+    if(sizeof($result2)>0){
+        echo "This username exist";
+    }
+    else{
+        $db->insert("user",[
+            "username"=>$username,
+            "password"=>$password,
+            "telephone"=>$telephone,
+            "status"=>1,
+            "fav"=>""
+        ]);
+        echo "finish";
+    }
+}
+?>
