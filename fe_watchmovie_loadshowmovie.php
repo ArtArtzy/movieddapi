1b9d6f248ad6f0f6207c7033b9fee2ffda69444e
Autarn Sukonnakorn <one.at.once@gmail.com>
2022-01-06 16:35:10 +0700

    edit func change catname

--- fe_watchmovie_loadshowmovie.php
+++ fe_watchmovie_loadshowmovie.php
@@ -1,14 +1,17 @@
 <?php
 require_once('connection.php');
+require_once('func_changecatname.php');
 $_POST = json_decode(file_get_contents("php://input"),true);
 
 $id=$_POST['id'];
-
-
-// $result2=$db->select("category","*");
+$id=48;
 
 $result=$db->select("movie","*",[
     "id"=>$id
 ]);
+
+$result[0]['type']=convertCatname($result[0]['type'],$db);
+
+ 
 echo json_encode($result);
 ?>
---
+++ func_changecatname.php
@@ -0,0 +1,27 @@
+<?php
+function convertCatname($allCat,$db){
+$textreturn="";
+$arrCat=explode(",",$allCat);
+
+for($i=0;$i<sizeof($arrCat);$i++){
+    $arrCat[$i]=trim($arrCat[$i],"[");
+    $arrCat[$i]=trim($arrCat[$i],"]");
+}
+$result=$db->select("category","*");
+
+for($i=0;$i<sizeof($arrCat);$i++){
+
+    for($j=0;$j<sizeof($result);$j++){
+
+        if($arrCat[$i]==$result[$j]['id']){
+
+            $str=$result[$j]['catname'];
+            $textreturn=$textreturn . "| " . $str . " ";
+        }
+    }
+ 
+}
+$textreturn=ltrim($textreturn,"|");
+return $textreturn;
+}
+?>
