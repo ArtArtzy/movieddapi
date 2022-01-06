93edce2ae38b74a463f788abbcc930af1b130d52
Autarn Sukonnakorn <one.at.once@gmail.com>
2022-01-06 16:40:44 +0700

    load show movie

--- fe_watchmovie_loadshowmovie.php
+++ fe_watchmovie_loadshowmovie.php
@@ -4,7 +4,7 @@ require_once('func_changecatname.php');
 $_POST = json_decode(file_get_contents("php://input"),true);
 
 $id=$_POST['id'];
-$id=48;
+
 
 $result=$db->select("movie","*",[
     "id"=>$id
