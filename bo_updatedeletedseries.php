325d2d6cece18eaf8b3b2806cfc92f877de11003
Autarn Sukonnakorn <one.at.once@gmail.com>
2022-01-07 17:56:13 +0700

    update deleted series (checkbox)

--- bo_updatedeletedseries.php
+++ bo_updatedeletedseries.php
@@ -3,7 +3,7 @@ require_once('connection.php');
 $_POST = json_decode(file_get_contents("php://input"),true);
 $value = $_POST['value'];
 $id = $_POST['id'];
-$db->update("deletedmovie",[
+$db->update("deletedseries",[
     "status"=>$value
 ],[
     "id"=>$id
